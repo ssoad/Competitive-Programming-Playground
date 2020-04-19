b279d1c8414051d50d6128af0a1190c8ea6a3a5a
Sohanuzzman Soad <mdsoad@gmail.com>
2020-04-19 23:40:48 +0600

    Create 584A-OlesyaAndRodion.cpp

---
+++ 584A-OlesyaAndRodion.cpp
@@ -0,0 +1,50 @@
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+#define soads_game int main()
+#define at(a,j) ((a>>j)&1)
+#define stop return 0;
+using namespace std;
+
+/*
+    Author- Sohanuzzaarrn Soad
+    University of Asia Pacific (UAP),BD
+
+*/
+soads_game
+{
+    int n, t;
+    cin>>n>>t;
+    if(t == 10)
+    {
+        if(n == 1)
+        {
+            cout<<"-1";
+            puts("-1");
+        }
+        else
+        {
+            cout<<"1";
+            for(int p = 0; p < n - 1; p++)
+            {
+                cout<<"0";
+            }
+        }
+    }
+    else
+    {
+        for(int p = 0; p < n; p++)
+        {
+            cout<<t;
+        }
+    }
+
+    stop
+}
+
+
+
+
+
+
