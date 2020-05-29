a085d6277f54efa580b56313dcf2966409bd362f
Sohanuzzman Soad <mdsoad@gmail.com>
2020-05-30 00:51:53 +0600

    Update 706B-Interesting drink.cpp

--- 706B-Interesting drink.cpp
+++ 706B-Interesting drink.cpp
@@ -1,49 +1,35 @@
 #include"iostream"
+#include "vector"
 #include"bits/stdc++.h"
 using namespace std;
 #define start int main()
 #define stop return 0
 
 /*
-Author-SohanuzzamanSoad
-UniversityofAsiaPacific(UAP),BD
+Author-Sohanuzzaman Soad
+University of Asia Pacific(UAP),BD
 
 */
 
-start {
-    int sh,dy;
-    string a="";
-    cin >> sh;
-    int shop[sh];
 
-    for(int i=0;i<sh;i++)
-    {
-        cin>>shop[i];
-    }
-    cin>>dy;
-    long long day[dy],day1[dy];
-    for(int i=0;i<dy;i++)
-    {
-        cin>>day[i];
-    }
-    sort(shop,shop+sh);
-    for(int i=0;i<dy;i++)
-    {
-       long long c=0;
-       for(int j=0;j<sh;j++)
-       {
-           if(day[i]<shop[j])
-           {
-               break;
-           }
-           else
-           {
-               c++;
-           }
-       }
-       cout<<c<<endl;
-       //day1[i]=c;
+
+int main(){
+
+    const int N = 100100;
+    long n; scanf("%ld\n", &n);
+    vector<long> rest(N + 1, 0);
+    for(long p = 0; p < n; p++){
+        long x; scanf("%ld", &x);
+        ++rest[x];
     }
 
+    for(long p = 1; p <= N; p++){rest[p] += rest[p - 1];}
+
+    long q; scanf("%ld\n", &q);
+    while(q--){
+        long a; scanf("%ld\n", &a);
+        printf("%ld\n", rest[(a <= N) ? a : N]);
+    }
+
+    return 0;
 }
-
